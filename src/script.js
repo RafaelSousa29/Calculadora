370a7182649298336dbc91f85278e8691133193c
RafaelSousa29 <brasousa29@gmail.com>
2024-07-02 00:57:30 +0100

    funcao calculadora completa

--- src/script.js
+++ src/script.js
@@ -1,11 +1,34 @@
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function Calculadora (num1, num2, operacao){
+  let num1 = parseint(num1)
+  let num2 = parseint(num2)
+  let resultado
 
-//ESCREVA SEU CÓDIGO AQUI!!
+  switch (operador){
+    case '+':
+      resultado = num1 + num2
+      break;
+      case '-':
+        resultado = num1 - num2
+        break;
+        case '/':
+          resultado = num1 / num2
+          break; 
+          case '*': 
+          resultado = num1 * num2
+          case 'e':
+            let contador = 0
+            resultado = num1 * num1
+            while (contador < num2){
+              resultado = resultado * num1
+              contador++
+            }
+            break;
+  }
 
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  if ( resultado == undefined || resultado > 1000000){
+    resultado = "ERRO"
+  }
+}
 
 let visor = document.getElementById("visor");
 
